147d827e2cded3d6411efee5ac0e44065fd6932e
VitoriaIzaine <86832458+VitoriaIzaine@users.noreply.github.com>
2021-10-18 08:16:52 -0300

    Primeiro commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <section class="main-section">
+
+        <div class="content">
+          <h2>
+            <span>Bem vindo a Pesquisa</span>
+            <span>de Satisfação</span>
+          </h2>
+          <h6>Dê seu feedback sobre o colégio</h6>
+          <button>Acessar Formulário</button>
+        </div>
+
+      <img src="./../assets/media/modelo.png" alt="" />
+        <div class="inner"></div>
+
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="sass">
+@import "scss/Home"
+</style>
